b729a1714b503355b0d705f6b9f68ccb70963f00
godtanatos <78075671+godtanatos@users.noreply.github.com>
2023-05-09 00:19:49 +0300

    feat: add solution

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,30 @@
-
+const fs = require("fs/promises");
+const path = require("path");
+async function copyDir(directory, copyDrectory) {
+  let direct = directory;
+  let copyDirect = copyDrectory;
+  await dellDir(copyDirect);
+  await fs.mkdir(copyDirect, { recursive: true });
+  let items = await fs.readdir(direct, { withFileTypes: true });
+  for (var i = 0; i < items.length; i++) {
+    if (items[i].isDirectory()) {
+      copyDir(
+        path.normalize(`${directory}/${items[i].name}`),
+        path.normalize(`${copyDirect}/${items[i].name}`)
+      );
+    } else {
+      await fs.copyFile(
+        path.normalize(direct + "/" + items[i].name),
+        path.normalize(copyDirect + "/" + items[i].name)
+      );
+    }
+  }
+}
+async function dellDir(directory) {
+  let delDirectory = directory;
+  await fs.rm(delDirectory, { force: true, recursive: true });
+}
+copyDir(
+  path.normalize(`${__dirname}/files`),
+  path.normalize(`${__dirname}/files-copy`)
+);
